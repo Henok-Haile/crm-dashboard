fcc49cd1caefc3ce898817b13bab9991eed3ed21
Henok-Haile <henokhaile53@gmail.com>
2025-05-06 19:00:39 +0300

    removed test files

--- __tests__/CreateCustomerForm.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import CreateCustomerForm from "../components/CreateCustomerForm";
-
-test("renders Add Customer button", () => {
-  render(<CreateCustomerForm onCustomerAdded={() => {}} />);
-  const button = screen.getByText("+ Add Customer");
-  expect(button).toBeInTheDocument();
-});
